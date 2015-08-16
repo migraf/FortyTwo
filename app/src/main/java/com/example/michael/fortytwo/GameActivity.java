420aa6921651c8ad26f3d9370f4d13aeecae5714
migraf <michael.m.graf@gmx.de>
2015-08-16 17:11:27 +0200

    the buttons now subtract from the inital value

--- app/src/main/java/com/example/michael/fortytwo/GameActivity.java
+++ app/src/main/java/com/example/michael/fortytwo/GameActivity.java
@@ -1,7 +1,9 @@
 package com.example.michael.fortytwo;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -11,49 +13,85 @@ import java.util.Random;
  * Created by Michael on 16.08.2015.
  */
 public class GameActivity extends Activity {
+    Button topLeftButton,bottomLeftButton,topRightButton,bottomRightButton;
+    TextView startNumber;
+    boolean gameOver = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game_layout);
 
         //Sets random numbers to all the Buttons
-        Button topLeftButton = (Button)findViewById(R.id.top_left_button);
-        topLeftButton.setText(String.valueOf(createRandomNumber(100)));
+        topLeftButton = (Button)findViewById(R.id.top_left_button);
+        topLeftButton.setText(String.valueOf(createRandomNumberInRange(1, 5)));
 
-        Button bottomLeftButton = (Button)findViewById(R.id.bottom_left_button);
-        bottomLeftButton.setText(String.valueOf(createRandomNumber(100)));
+        bottomLeftButton = (Button)findViewById(R.id.bottom_left_button);
+        bottomLeftButton.setText(String.valueOf(createRandomNumberInRange(20, 50)));
 
-        Button topRightButton = (Button)findViewById(R.id.top_right_button);
-        topRightButton.setText(String.valueOf(createRandomNumber(100)));
+        topRightButton = (Button)findViewById(R.id.top_right_button);
+        topRightButton.setText(String.valueOf(createRandomNumberInRange(5, 20)));
 
-        Button bottomRightButton = (Button)findViewById(R.id.bottom_right_button);
-        bottomRightButton.setText(String.valueOf(createRandomNumber(100)));
+        bottomRightButton = (Button)findViewById(R.id.bottom_right_button);
+        bottomRightButton.setText(String.valueOf(createRandomNumberInRange(50, 100)));
 
         //set the random number to subtract from
 
-        TextView startNumber = (TextView) findViewById(R.id.initial_number);
-        startNumber.setText(String.valueOf(createRandomNumber(300)));
+        startNumber = (TextView) findViewById(R.id.initial_number);
+        startNumber.setText(String.valueOf(createRandomNumberInRange(100, 300)));
+
+
+
+
 
 
 
 
     }
 
-    private static int createRandomNumberSmall (int range ){
+    private static int createRandomNumberInRange (int top, int bottom ){
+
 
         Random random = new Random();
 
-        return random.nextInt(range);
+        int range =  top -bottom +1;
+        int number =(int) (range*random.nextDouble())+ bottom;
+
+        return number;
+
+
+
 
     }
-    private static int createRandomNumber (int range ){
 
-        Random random = new Random();
 
-        return random.nextInt(range);
+    public void onLeftButtonClick(View view) {
+        subtractButtonValue(startNumber,topLeftButton);
 
     }
 
+    public void onRightButton1Click(View view) {
+        subtractButtonValue(startNumber,topRightButton);
+    }
 
+
+    public void onBottomLeftButtonClick(View view) {
+        subtractButtonValue(startNumber,bottomLeftButton);
+    }
+
+    public void onBottomRightButtonClick(View view) {
+        subtractButtonValue(startNumber,bottomRightButton);
+    }
+
+    public void subtractButtonValue(TextView number, Button button){
+        int buttonValule = Integer.parseInt((String) button.getText());
+        int textValue = Integer.parseInt((String) number.getText());
+        int newValue =  textValue-buttonValule;
+
+        number.setText(String.valueOf(newValue));
+
+
+
+    }
 }
