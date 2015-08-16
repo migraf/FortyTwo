2c75cab10ff41fb492c4fbd32352fc82da244360
migraf <michael.m.graf@gmx.de>
2015-08-16 16:22:02 +0200

    added numbers to the buttons.

--- app/src/main/java/com/example/michael/fortytwo/GameActivity.java
+++ app/src/main/java/com/example/michael/fortytwo/GameActivity.java
@@ -2,6 +2,10 @@ package com.example.michael.fortytwo;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
 
 /**
  * Created by Michael on 16.08.2015.
@@ -9,7 +13,42 @@ import android.os.Bundle;
 public class GameActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game_layout);
+
+        //Sets random numbers to all the Buttons
+        Button topLeftButton = (Button)findViewById(R.id.top_left_button);
+        topLeftButton.setText(String.valueOf(createRandomNumber(100)));
+
+        Button bottomLeftButton = (Button)findViewById(R.id.bottom_left_button);
+        bottomLeftButton.setText(String.valueOf(createRandomNumber(100)));
+
+        Button topRightButton = (Button)findViewById(R.id.top_right_button);
+        topRightButton.setText(String.valueOf(createRandomNumber(100)));
+
+        Button bottomRightButton = (Button)findViewById(R.id.bottom_right_button);
+        bottomRightButton.setText(String.valueOf(createRandomNumber(100)));
+
+        //set the random number to subtract from
+
+        TextView startNumber = (TextView) findViewById(R.id.initial_number);
+        startNumber.setText(String.valueOf(createRandomNumber(300)));
+
+
+
+
     }
+
+    private static int createRandomNumber (int range ){
+
+        Random random = new Random();
+
+        return random.nextInt(range);
+
+
+
+    }
+
+
 }
