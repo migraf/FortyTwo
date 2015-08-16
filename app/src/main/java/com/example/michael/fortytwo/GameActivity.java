783d8680628460e191cdfa6318c568856e51cb83
migraf <michael.m.graf@gmx.de>
2015-08-16 14:24:10 +0200

    basic layout

---
+++ app/src/main/java/com/example/michael/fortytwo/GameActivity.java
@@ -0,0 +1,15 @@
+package com.example.michael.fortytwo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Michael on 16.08.2015.
+ */
+public class GameActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.game_layout);
+    }
+}
---
+++ app/src/main/java/com/example/michael/fortytwo/SettingsActivity.java
@@ -0,0 +1,11 @@
+package com.example.michael.fortytwo;
+
+import android.app.Activity;
+
+/**
+ * Created by Michael on 16.08.2015.
+ */
+public class SettingsActivity extends Activity{
+
+
+}
